986943dcf1d7d0c8900cfe6d36abc7243bc2c61b
Angela ordonez <angela.ordonez@yuxiglobal.com>
2017-07-24 15:49:30 -0500

    compact table created

--- src/app/leaderboard/leaderboard.module.ts
+++ src/app/leaderboard/leaderboard.module.ts
@@ -4,13 +4,14 @@ import { FullTableComponent } from './full-table/full-table.component';
 import { LeaderboardComponent } from './leaderboard.component';
 
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CompactTableComponent } from './compact-table/compact-table.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NgxDatatableModule
   ],
-  declarations: [FullTableComponent, LeaderboardComponent],
+  declarations: [FullTableComponent, LeaderboardComponent, CompactTableComponent],
   exports: [LeaderboardComponent]
 })
 export class LeaderboardModule {}
